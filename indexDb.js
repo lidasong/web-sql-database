f44de826eb3f6600482c202837e116ceeb4f4a7f
lidasong <lidasong2014@163.com>
2015-11-20 10:44:44 +0800

    indexedDB

--- indexDb.js
+++ indexDb.js
@@ -17,6 +17,11 @@
         this.storeObjects = {};
     }
 
+    IndexDB.prototype = {
+    	createDb:create,
+    	createStore:createStore
+    };
+
     function create(dbName, version, options) {
         version = version || 1;
         var openRequest = this.indexedDB.open(dbName, version);
@@ -35,29 +40,40 @@
                 console.log('open IndexDB Error');
             }
         }
-
+        return openRequest;
     }
 
     function createStore(storeName, options) {
+    	var store;
         if (this.db.objectStoreNames.contain(storeName)) {
             return 'the store ' + storeName ' has created';
         }
-        this.db.createObjectStore(storeName, options);
-        this.storeObjects[storeName] = new StoreObject(this.db,storeName)
+        store = this.db.createObjectStore(storeName, options);
+        this.storeObjects[storeName] = new StoreObject(this.db,storeName,store)
+        return this.storeObjects[storeName];
     }
 
 
-    function StoreObject(db, storeName) {
+    function StoreObject(db, storeName,store) {
         this.storeName = storeName;
         this.db = db;
+        this.store = store;
     }
 
     StoreObject.prototype = {
+    	createIndex:createIndex,
     	add:add,
     	get:get,
     	put:put,
     	delete:delete
     };
+    
+    function createIndex(options){
+    	var indexName = options.indexName,
+    		indexAttr = options.attribute,
+    		unique = options.unique;
+    	this.store.createIndex(indexName,indexAttr,{unique:unique});
+    }
 
     function add(options) {
         var transction, storeName,store,request;
@@ -86,11 +102,39 @@
     }
 
     function getAll(options){
+    	_getSection(options);
+    }
+
+    function getRange(options){
+    	var lower,lowerOpen,upper,upperOpen,
+    		isLower,isUpper,isSection,range;
+    	isLower = !!options.lower;
+    	isUpper = !!options.upper;
+    	isSection = isLower && isUpper;
+    	if(options.lower){
+	    	lower = options.lower.value,
+    		lowerOpen = options.lower.open,
+    	}
+    	if(options.upper){
+    		upper = options.upper.value,
+    		upperOpen = options.upper.open;
+    	}
+    	if(isSection){
+    		range = IDBKeyRange.bound(upper,lower,upperOpen,lowerOpen);
+    	}else if(isLower){
+    		range = IDBKeyRange.lowerBound(lower,lowerOpen);
+    	}else{
+    		range = IDBKeyRange.upperBound(upper,upperOpen);
+    	}
+    	_getSection(options,range);
+    }
+
+    function _getSection(options,range){
     	var transaction,storeName,store,result = [];
     	storeName = [this.storeName];
     	transaction = this.db.transaction(storeName,'readonly');
     	store = transaction.objectStore(this.storeName);
-    	store.openCursor().onsuccess = function(evt){
+    	store.openCursor(range).onsuccess = function(evt){
     		var cursor = evt.target.result;
     		if(cursor){
     			result.push(cursor);
@@ -122,34 +166,25 @@
 
 
     function operateStore(options){
-    	var transction, storeName,store,request,type,pattern;
-    	type = options.type;
-    	pattern = options.pattern;
-    	storeName = [this.storeName];
-    	transction = this.db.transaction(storeName, pattern);
-    	store = transction.objectStore(this.storeName);
+    	var type = options.type;
     	switch(type){
     		case 'add':
-		    	request = store.add(options.data);
+		    	this.add(options.key,options);
 		    	break;
     		case 'get':
-    			if(options.index){
-    				request = store.index(options.index).get(options.key);
-    			}else{
-    				request = store.get(options.key);
-    			}
+    			this.get(options.key,options)
     			break;
+    		case 'getAll':
+    			this.getAll(options);
     		case 'delete':
-    			request = store.delete(options.key);
+    			this.delete(options.key,options);
     			break;
     		case 'put':
-    			request = store.put(options.data,options.key);
+    			this.put(options.key,options);
     			break;
     		default:
     			break;
     	}
-    	request.onsuccess = options['success'].call(this);
-    	request.onerror = options['error'].call(this);
     }
 
 });
